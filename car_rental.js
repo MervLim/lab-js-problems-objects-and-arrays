8a7aa6647b512f288ae1a8532d6c7223968a3bbf
Mervyn Lim <MervynLim@Mervyns-MacBook-Pro.local>
2017-05-23 12:03:06 +0800

    did assignment until rentCar function

--- car_rental.js
+++ car_rental.js
@@ -1,11 +1,41 @@
 // Customer Object
 var Customer = function (customerInfo) {
-  // ** your code here**
+  this.name = customerInfo.name;
+  this.id = customerInfo.id;
+  this.carRented = null;
 };
 
 // Car Object
 var Car = function (carInfo) {
-  // ** your code here**
+  this.id = carInfo.id;
+  this.producer = carInfo.producer;
+  this.model = carInfo.model;
+  this.rentalPrice = carInfo.rentalPrice;
+  this.available = true;
+  this.rentalDuration = 0;
+  this.customer = null;
+
+  this.quotePrice = function(rentalDuration){
+    var quotePrice = rentalDuration * this.rentalPrice;
+    return quotePrice;
+
+  }
+  this.reserve = function(customer, rentalDuration){
+    if (this.available === true) {
+      this.available = false;
+      this.customer = customer;
+      this.rentalDuration = rentalDuration;
+      return true;
+    }
+    else {
+      return false;
+    }
+
+
+  }
+  this.return = function () {
+
+  }
 };
 
 // Vendor Object
@@ -38,7 +68,56 @@ var Vendor = function(name) {
     });
   };
 
-  // **your code here**
+  this.addCar = function(carObj) {
+    if(this.getCar(carObj.id)){
+      console.log('ID already exists')
+    } else {
+      this.cars.push(carObj);
+      console.log('Car added to warehouse');
+    }
+  }
+  this.addCustomer = function(customerObj){
+    if (this.getCustomer(customerObj.id)){
+      console.log('ID already exists');
+    } else {
+      this.customers.push(customerObj)
+      console.log('Customer added to warehouse');
+    }
+  }
+
+  this.removeCar = function (carID) {
+    var carIndex = this.findCarIndex(carID)
+    if (carIndex >= 0) {
+      this.cars.splice(carIndex, 1);
+    } else {
+      console.log('Car not found');
+    }
+  }
+
+  this.removeCustomer = function (customerID) {
+    var customerIndex = this.findCustomerIndex(customerID){
+      if (customerIndex >= 0) {
+        this.customers.splice(customerIndex, 1);
+      } else {
+        console.log('Customer not found');
+      }
+    }
+  }
+this.availableCars = function (){
+  function(car){
+
+
+  };
+}
+this.rentCar = function (customerID, rentalDuration){
+  if (this.availableCars.length === 0){
+    console.log('All our cars have been rented');
+  } else {
+    if (this.getCustomer(customerID)){
+
+    }
+  }
+}
 };
 
 
@@ -47,6 +126,8 @@ var customerInfo = {
   id: "001",
   name: "Sherman"
 };
+
+
 var customerA = new Customer(customerInfo);
 
 var carInfo = {
